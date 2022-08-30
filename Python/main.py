24348eb3b23fe0b1379018fdd66c028fbc8f8f6b
Daniele Sisinni <daniele.sisinni@edu.unito.it>
2022-08-30 16:28:43 +0200

    modifiche python

--- Python/main.py
+++ Python/main.py
@@ -19,54 +19,54 @@ def main():
         return
 
     scelta = input("Scegli le opzioni:\n1) Genera una nuova matrice casuale \n2) Leggi la matrice dal file "
-                   "/matrix.txt \n ")
+                   "/matrice.txt \n ")
 
     if scelta == '2':
         print("Leggo la matrice...")
-        with open('matrix.txt', 'r') as f_matrix:
+        with open('matrice.txt', 'r') as f_matrice:
             #leggo ogni numero in una riga e la metto in una lista e poi per ogni line e lista di liste, matrice
-            matrix = [[int(num) for num in line.split(' ')] for line in f_matrix]
+            matrice = [[int(num) for num in line.split(' ')] for line in f_matrice]
     else:
         print("Genero la matrice...")
-        matrix = ([random.choices(range(0, 2), k=num_colonne) for _ in range(num_righe)])
-        with open('matrix.txt', 'w') as out:
-            for riga in matrix:
+        matrice = ([random.choices(range(0, 2), k=num_colonne) for _ in range(num_righe)])
+        with open('matrice.txt', 'w') as out:
+            for riga in matrice:
                 out.write(' '.join([str(a) for a in riga]) + '\n')
 
-    ruot90 = ruotare_matrix(pattern)
-    ruot180 = ruotare_matrix(ruot90)
-    ruot270 = ruotare_matrix(ruot180)
+    ruot90 = ruotare_matrice(pattern)
+    ruot180 = ruotare_matrice(ruot90)
+    ruot270 = ruotare_matrice(ruot180)
 #li casto a lista
-    res = check_matrix(list(trova_pattern(matrix, pattern)))
-    res_90 = check_matrix(list(trova_pattern(matrix, ruot90)))
-    res_180 = check_matrix(list(trova_pattern(matrix, ruot180)))
-    res_270 = check_matrix(list(trova_pattern(matrix, ruot270)))
+    check = check_matrice(list(trova_pattern(matrice, pattern)))
+    check_90 = check_matrice(list(trova_pattern(matrice, ruot90)))
+    check_180 = check_matrice(list(trova_pattern(matrice, ruot180)))
+    check_270 = check_matrice(list(trova_pattern(matrice, ruot270)))
 
-    if res or res_90 or res_180 or res_270:
+    if check or check_90 or check_180 or check_270:
         print("Pattern individuato,\n")
-        if res:
+        if check:
             print("Rotazione: 0 gradi")
-        if res_90:
+        if check_90:
             print("Rotazione: 90 gradi")
-        if res_180:
+        if check_180:
             print("Rotazione: 180 gradi")
-        if res_270:
+        if check_270:
             print("Rotazione: 270 gradi")
     else:
         print("Pattern non presente")
 
 
-def ruotare_matrix(matrix):
+def ruotare_matrice(matrice):
     #zip lo usiamo per avere una lista di tuple
-    lista_di_tuple = zip(*matrix[::-1])
+    lista_di_tuple = zip(*matrice[::-1])
     #Forzatura cast a list per ogni elemento della lista di tuple
     return [list(elem) for elem in lista_di_tuple]
 
 
-def check_matrix(matrix):
+def check_matrice(lista):
     res = False
-    for i in range(len(matrix)):
-        if True in matrix[i]:
+    for i in range(len(lista)):
+        if True in lista[i]:
             res = True
             break
     return res
@@ -74,14 +74,14 @@ def check_matrix(matrix):
 #LISTA DI true o false, uno per ogni sottomatrice, Yield genera al volo i valori e li ritorna.
 # Ritorna matrice di matrici
 #Per ogni riga della matrice di valori vado a controllare il valoro dentro la matrice di valori
-def trova_pattern(matrix, pattern):
-    sotto_matrice_di_matrici = generazione_finestre(matrix, (len(pattern), len(pattern[0])))
-    for riga_sotto_matrice in sotto_matrice_di_matrici:
+def trova_pattern(matrice, pattern):
+    matrice_di_sotto_matrici = generazione_finestre(matrice, (len(pattern), len(pattern[0])))
+    for riga_sotto_matrice in matrice_di_sotto_matrici:
         yield [sotto_matrice == pattern for sotto_matrice in riga_sotto_matrice]
 
 #Lista di sottomatrici delle dimensioni del pattern. 
-def generazione_finestre(matrix, formato_finestra):
-    altezza, larghezza = len(matrix), len(matrix[0])
+def generazione_finestre(matrice, formato_finestra):
+    altezza, larghezza = len(matrice), len(matrice[0])
     finestra_a, finestra_l = formato_finestra
 
     #Generazione di righe e colonne
@@ -89,7 +89,7 @@ def generazione_finestre(matrix, formato_finestra):
         #Genera Lista di matrici per y in range
         yield [
             #una porzione di riga in una porzione della matrice
-            [riga[x:x + finestra_l] for riga in matrix[y:y + finestra_a]]
+            [riga[x:x + finestra_l] for riga in matrice[y:y + finestra_a]]
             for x in range(larghezza - finestra_l + 1)
         ]
 
